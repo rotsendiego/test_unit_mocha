0eaf95e1e073b29476b71074d707980f14aebe56
RotseDiego <rotsen.albuquerque@ab-inbev.com>
2022-10-15 16:20:24 -0300

    first tc

---
+++ basicTest.js
@@ -0,0 +1,15 @@
+var assert = require("assert");
+
+describe("BasicTest", function() {  
+
+    describe("Multiplicacao", function() {
+
+        it("should equal 15 when 5 is multiple by 3", function() { 
+
+            var result = 5 * 3;
+            assert.equal(result, 15)
+        });
+        
+    });
+
+});
